8db5bc86a061fb5b6712ce8dd8a3b7b43f6cfb55
Arish5Kannan <arishkannan40@gmail.com>
2024-10-06 05:53:32 -0700

    collection added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request , "shop/index.html" )
 def register(request):
-    return render(request , "shop/register.html")    
+    return render(request , "shop/register.html")  
+def collection(request):
+    catagory = Category.objects.filter(status=0)
+    return render(request,"shop/collections.html",{"catagory":catagory})      
