7a330cc36631f254c7f4188f7c13c49986360654
Arish5Kannan <arishkannan40@gmail.com>
2024-10-08 09:57:39 -0700

    third commit

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request , "shop/index.html" )
@@ -7,4 +8,11 @@ def register(request):
     return render(request , "shop/register.html")  
 def collection(request):
     catagory = Category.objects.filter(status=0)
-    return render(request,"shop/collections.html",{"catagory":catagory})      
+    return render(request,"shop/collections.html",{"catagory":catagory})
+def products(request,name):
+    if(Category.objects.filter(status=0,name=name)):
+        prod = Product.objects.filter(category__name=name) 
+        return render(request,"shop/products.html",{"prod":prod})        
+    else:
+        messages.warning(request,"No Such Category Found")
+        return redirect('shop/collections.html')
